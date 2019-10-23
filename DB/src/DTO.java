3fdeb5b8d9a15425f8cf55384bd2c6f9fd6c457d
daniel-fitzg <dan8790@icloud.com>
2019-10-24 00:00:02 +0100

    Added createTable method in DB class

--- DB/src/DTO.java
+++ DB/src/DTO.java
@@ -1,29 +1,47 @@
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
-//added imports to classpath
+
 public class DTO {
+    private Cluster cluster;
+    private Session session;
 
-
-    public static void main (String[] args) {
-        Cluster cluster;
-        Session session;
-
+    DTO() {
         cluster = Cluster.builder().addContactPoint("127.0.0.1").withPort(9042).build();
         session = cluster.connect();
-        createKeyspace("test_keyspace", "SimpleStrategy", 1, session);
+    }
+
+    public static void main (String[] args) {
+        new DTO().executeQuerySet();
+    }
+
+    private void executeQuerySet() {
+        createKeyspace(session, "test_keyspace", "SimpleStrategy", 1);
+        createTable(session, "test_keyspace", "test_table");
         session.close();
         cluster.close();
     }
 
-
-    public static void createKeyspace(String keyspaceName, String replicationStrategy, int replicationFactor, Session session) {
-        StringBuilder sb = new StringBuilder("CREATE KEYSPACE IF NOT EXISTS ")
+    private void createKeyspace(Session session, String keyspaceName, String replicationStrategy, int replicationFactor) {
+        StringBuilder stringBuilder = new StringBuilder("CREATE KEYSPACE IF NOT EXISTS ")
                 .append(keyspaceName).append(" WITH replication = {")
                 .append("'class':'").append(replicationStrategy)
                 .append("','replication_factor':").append(replicationFactor)
                 .append("};");
 
-        String query = sb.toString();
+        String query = stringBuilder.toString();
+        session.execute(query);
+    }
+
+    private void createTable(Session session, String keyspace, String tableName) {
+        // TODO: Use keyspace command not working
+        StringBuilder stringBuilder = new StringBuilder("USE " + keyspace + "CREATE TABLE IF NOT EXISTS ")
+                .append(tableName).append("(")
+                .append("id text, ")
+                .append("title text, ")
+                .append("age int, ")
+                .append("PRIMARY KEY(id));");
+
+        String query = stringBuilder.toString();
         session.execute(query);
     }
 }
