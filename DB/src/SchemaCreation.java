b40e3b2d9e685a2bc020a84a51f4a535d4e8409e
daniel-fitzg <dan8790@icloud.com>
2019-10-28 21:56:35 +0000

    Added read and write methods

--- DB/src/SchemaCreation.java
+++ DB/src/SchemaCreation.java
@@ -1,12 +1,13 @@
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
 
 public class SchemaCreation {
     private Cluster cluster;
     private Session session;
 
-    SchemaCreation() {
+    private SchemaCreation() {
         cluster = Cluster.builder().addContactPoint("127.0.0.1").withPort(9042).build();
         session = cluster.connect();
     }
@@ -16,10 +17,18 @@ public class SchemaCreation {
     }
 
     private void executeQuerySet() {
-        createKeyspace(session, "test_keyspace", "SimpleStrategy", 1);
-        createTable(session, "test_keyspace", "test_table");
-        session.close();
-        cluster.close();
+        try {
+            createKeyspace(session, "test_keyspace", "SimpleStrategy", 1);
+            createTable(session, "test_keyspace", "test_table");
+            write("test_table");
+            read("test_table");
+            session.close();
+            cluster.close();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            session.close();
+            cluster.close();
+        }
     }
 
     private void createKeyspace(Session session, String keyspaceName, String replicationStrategy, int replicationFactor) {
@@ -34,9 +43,30 @@ public class SchemaCreation {
         session.execute(preparedStatement.bind());
 
         preparedStatement = session.prepare("CREATE TABLE IF NOT EXISTS " + tableName +
-                " (id text, title text, age int, PRIMARY KEY(id));");
+                " (id text, name text, age int, PRIMARY KEY(id));");
 
         session.execute(preparedStatement.bind());
+    }
+
+    private void write(String tableName) {
+        PreparedStatement preparedStatement = session.prepare("INSERT INTO " + tableName + " (id, name, age) " +
+                "VALUES ('1234', 'Daniel', 32)");
+
+        session.execute(preparedStatement.bind());
+
+        preparedStatement = session.prepare("INSERT INTO " + tableName + " (id, name, age) " +
+                "VALUES ('9876', 'Bob', 57)");
+
+        session.execute(preparedStatement.bind());
+    }
+
+    private void read(String tableName) {
+        PreparedStatement preparedStatement = session.prepare("SELECT * FROM " + tableName);
+        ResultSet resultSet = session.execute(preparedStatement.bind());
+
+        resultSet.forEach(result -> {
+            System.out.println("Data: " + result);
+        });
     }
 }
 
