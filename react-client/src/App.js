43b6515fb18e59b97732e7a3757130f51bee48bb
daniel-fitzg <dan8790@icloud.com>
2020-01-31 22:40:16 +0000

    Added TitleHeader.js component

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TitleHeader from "./main-frame-components/TitleHeader"
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <TitleHeader />
         <h3>Main frame for the app</h3>
       </div>
     )
