1c116904a95411b6df4c7f30f1c5a10b7393347a
daniel-fitzg <dan8790@icloud.com>
2020-03-22 13:49:20 +0000

    Optimised GetUserConversationsServlet

--- tomcat-server/src/GetUserConversationsServlet.java
+++ tomcat-server/src/GetUserConversationsServlet.java
@@ -1,17 +1,13 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicReference;
 
 /*
 * Servlet gets and returns the conversations to which a single user belongs
@@ -23,46 +19,29 @@ public class GetUserConversationsServlet extends HttpServlet {
     // TODO Large scale refactor needed of this servlet
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Needed to circumvent Cross Origin Resource-Sharing
+        // Allows resource sharing across different origins
         response.setHeader("Access-Control-Allow-Origin", "*");
 
-        BufferedReader bufferedReader = new BufferedReader(request.getReader());
-        String incomingJsonString = bufferedReader.readLine();
-        JSONParser parser = new JSONParser();
-        JSONObject jsonObject = null;
-        try {
-            jsonObject = (JSONObject) parser.parse(incomingJsonString);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        }
+        ServletHelper servletHelper = new ServletHelper();
+        JSONObject incomingJsonObject = servletHelper.parseIncomingJSON(request, response);
 
-        UUID userId = UUID.fromString((String) jsonObject.get("userId"));
+        UUID userId = UUID.fromString((String) incomingJsonObject.get("userId"));
 
         CassandraDataStore cassandraDataStore = new CassandraDataStore();
 
-        // If userId belongs to a newly registered user, all users are returned to allow the new user to start a conversation.
-        // In reality, this method would be replaced by the users contact book sync to generate available users to interact with
-       // boolean isNewUser = cassandraDataStore.isNewUser(userId);
-
         List<User> users = cassandraDataStore.getAllUsers();
         User duplicateUser = null;
-        // Removes the current user from the user list to be sent back to the client
-//        users.forEach(user -> {
-//           if (user.getUserId().equals(userId)) {
-//               duplicateUser.set(user);
-//           }
-//        });
 
+        // Identifies and removes the current user within the returned list of all registered users
         for (User user : users) {
             if (user.getUserId().equals(userId)) {
                 duplicateUser = user;
                 break;
             }
         }
-
         users.remove(duplicateUser);
 
+        // Populate JSON array with user objects
         JSONArray jsonArray = new JSONArray();
         users.forEach(user -> {
             JSONObject userJsonObject = new JSONObject();
@@ -73,9 +52,7 @@ public class GetUserConversationsServlet extends HttpServlet {
             jsonArray.add(userJsonObject);
         });
 
-        response.getWriter().write(jsonArray.toJSONString());
-        response.getWriter().flush();
-        response.getWriter().close();
+        servletHelper.writeJsonOutput(response, jsonArray.toString());
 
         cassandraDataStore.close();
     }
